3cd0bac4a79a16803356efa8c02450792338525b
Nont Nonnipat <pinozen.thailand@gmail.com>
2025-10-04 16:14:45 +0700

    Add initial project structure for SingularityPlugin including .gitignore, pom.xml, and main plugin class

---
+++ src/main/java/com/example/SingularityPlugin.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import com.github.pinont.singularitylib.plugin.CorePlugin;
+
+public final class SingularityPlugin extends CorePlugin {
+
+    @Override
+    public void onPluginStart() {
+
+    }
+
+    @Override
+    public void onPluginStop() {
+
+    }
+}
